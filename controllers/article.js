478cff834672578c9973a8c38bf05c35dbc22166
Vincent Song <songvc@users.noreply.github.com>
2016-07-14 16:50:48 -0700

    takeout rollup for now

--- controllers/article.js
+++ controllers/article.js
@@ -2,61 +2,70 @@ import User from '../models/user';
 import Article from '../models/article';
 import config from '../config';
 
-const createArticle = (req, res) => {
+const createArticle = (req, res, next) => {
   console.log(req.body);
   const { title, tags ,text } = req.body;
 
   //create an article
-  const article = new Article({
+  const savedArticle = new Article({
     title,
     tags,
     text
   });
 
-  article.save(function (err) {
-    if (!err) {
-      return console.log('article created');
-    } else {
-      return console.log(err);
-    }
-  });
+  article.saveAsync()
+    .then(savedArticle => res.json(savedArticle))
+    .error(e => next(e));
+  // (function (err) {
+  //   if (!err) {
+  //     return console.log('article created');
+  //   } else {
+  //     return console.log(err);
+  //   }
+  // });
 }
 
-const deleteArticle = (req, res) => {
-
+const deleteArticle = (req, res, next) => {
+  console.log(req.body);
+  // const { id }
 }
 
-const getArticles = (req, res) => {
+const getArticles = (req, res, next) => {
   console.log(req.body);
   const { limit = 50, skip = 0 } = req.query;
-  Article.find({}, function (err, articles) {
-    if (err) {
-      return console.log(err);
-    }
-    res.fo
-  })
-}
-
-const getArticle = (req, res) => {
-  console.log(req.body);
-
-}
-
-const updateArticle = (req, res) => {
-  console.log(req.body);
-  const { id } = req.body.params;
-  // update an existing article
-  Article.findById(id, function(err, article) {
-    const { title, tags ,text } = req.body;
-
-    article.save(function(err) {
-      if (!err) {
-        return console.log('article updated');
-      } else {
-        return console.log(err);
-      }
+  Article.findAsync()
+    .then(function(articles) {
+      res.json(articles);
     })
-  })
+    .catch(next)
+    .error(console.error);
+}
+
+const getArticle = (req, res, next) => {
+  console.log(req.body);
+
+}
+
+const updateArticle = (req, res, next) => {
+  // console.log(req.body);
+  // const { id } = req.body.params;
+  // // update an existing article
+  // Article.findByIdAsync()
+  //   .then
+  //
+  //
+  //
+  // (id, function(err, article) {
+  //   const { title, tags ,text } = req.body;
+  //
+  //   article.save(function(err) {
+  //     if (!err) {
+  //       return console.log('article updated');
+  //     } else {
+  //       return console.log(err);
+  //     }
+  //   })
+  // })
 
 }
 
